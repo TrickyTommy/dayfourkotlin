2b922136d0bb9eb2550f8f4ea3b1b11f0e3c0739
TrickyTommy <trickytommy4@gmail.com>
2020-11-05 16:27:38 +0700

    akhir

--- src/main/kotlin/dayfour/compiletimeconstant/function/mainApplication.kt
+++ src/main/kotlin/dayfour/compiletimeconstant/function/mainApplication.kt
@@ -1,2 +1,33 @@
 package dayfour.compiletimeconstant.function
 
+fun main(){
+    val result = sumAll(1,2,3,4,5,6)
+    println(result)
+    ////
+    //(1..10) infix
+
+    val parent = Person("parent")
+    val child = Person("child")
+
+    parent children child
+    print(parent.list.map { it.name })
+}
+
+fun sumAll(vararg numbers: Int): Int {
+
+    return numbers.reduce{acc, i -> acc + i}
+
+
+}
+
+///baru
+class Person(val name: String){
+    private val _list = mutableListOf<Person>()
+    val list get() = _list
+
+
+    infix fun children(person: Person){
+        _list.add(person)
+    }
+
+}
